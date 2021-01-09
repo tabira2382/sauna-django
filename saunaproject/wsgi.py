e027af69e629c5b3e32121c06b613b04b87b37eb
tabira2382 <tabira2382@gmail.com>
2021-01-09 23:02:25 +0900

    wsgi変更

--- saunaproject/wsgi.py
+++ saunaproject/wsgi.py
@@ -1,18 +1,18 @@
-# import os
-
-# from dj_static import Cling
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saunaproject.settings")
-
-# application = Cling(get_wsgi_application())
-
-
-
 import os
 
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saunaproject.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
+
+
+
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saunaproject.settings")
+
+# application = get_wsgi_application()
